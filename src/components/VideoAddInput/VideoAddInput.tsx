171c05a8e87d84ce7b3aeb915da4f360f8dccb1b
DH <kdh6429@yahoo.co.kr>
2020-05-25 02:26:00 +0900

    change video input label

--- src/components/VideoAddInput/VideoAddInput.tsx
+++ src/components/VideoAddInput/VideoAddInput.tsx
@@ -17,10 +17,10 @@ export const VideoAddInput= () => {
     return (
         <>
         <div style={{marginTop: '50px', fontSize: '20px', fontWeight: 'bold', color: 'chartreuse', textAlign: 'center'}}>
-            ADD Youtube
+            ADD Music
         </div>
         <div style={{marginBottom: '20px', marginTop: '10px', display: 'flex', justifyContent: 'center'}}>
-            <div style={{float: 'left'}}><input type="text" style={{width: '500px', fontSize: '25px'}} value={videoId} placeholder="Youtube Video ID (예시: JgC_Kb5TYsU) " onChange={inputChange}/></div>
+            <div style={{float: 'left'}}><input type="text" style={{width: '500px', fontSize: '25px'}} value={videoId} placeholder="Youtube Video Link(or ID)" onChange={inputChange}/></div>
             <div style={{float: 'left'}}><button style={{fontSize: '25px'}} onClick={addVideo}>add</button></div>
         </div>
         </>
